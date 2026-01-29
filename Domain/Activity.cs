9d7fe19afd934c1f135f95e5123e21fb93abcf97
Jakub Jašek <kuba.jasek@email.cz>
2026-01-29 11:54:24 +0100

    End of section 2

---
+++ Domain/Activity.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.VisualBasic;
+
+namespace Domain;
+
+public class Activity   
+{
+    public String Id { get; set; } = Guid.NewGuid().ToString();
+    public required string Title { get; set; }
+    public DateTime Date { get; set; }
+    public required string Description { get; set; }
+    public required string Category { get; set; }
+    public bool IsCancelled { get; set; }
+
+    //location props
+    public required string City { get; set; }
+    public required string Venue { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+
+
+
+
+
+}
