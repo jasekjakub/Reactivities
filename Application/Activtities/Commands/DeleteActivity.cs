c5809cd30756095548d7ec0ffbce59a6a8e372c0
Jakub Jašek <kuba.jasek@email.cz>
2026-02-02 14:15:54 +0100

    End of Section 4

---
+++ Application/Activtities/Commands/DeleteActivity.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Application.Activtities.Commands;
+
+public class Delete
+{
+
+}
---
+++ Application/Activtities/Queries/GetActivityList.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Domain;
+
+namespace Application.Activtities.Queries;
+
+public class GetActivityList
+{
+    public class Query : IRequest<List<Activity>>{}
+
+    public class Handler (AppDbContext context) : IRequestHandler<Query, List<Activity>>
+    {
+        public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            return await context.Activities.ToListAsync(cancellationToken);
+        }
+    }
+}
