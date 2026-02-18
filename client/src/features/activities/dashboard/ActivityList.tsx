974055b4bc77990a5e3514f98fd57ef8ae3d0d59
Jakub Jašek <kuba.jasek@email.cz>
2026-02-18 15:46:41 +0100

    End of Section 13

--- client/src/features/activities/dashboard/ActivityList.tsx
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -2,22 +2,21 @@ import { Box, Typography } from "@mui/material";
 import ActivityCard from "./ActivityCard";
 import { useActivities } from "../../../lib/hooks/useActivities";
 
-
 export default function ActivityList() {
-  const { activities, isPending } = useActivities();
+    const { activities, isLoading } = useActivities();
 
-  if (!activities||isPending) return <Typography>Loading...</Typography>
-  
-  return (
-    <Box sx={{display:"flex", flexDirection: "column", gap: 3}}>
-        {activities.map(activity => (
-            <ActivityCard 
-            key={activity.id} 
-            activity ={activity} 
-            />
-        ))} 
+    if (isLoading) return <Typography>Loading...</Typography>
 
-    
-    </Box>
-  )
+    if (!activities) return <Typography>No activities found</Typography>
+
+    return (
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
+            {activities.map(activity => (
+                <ActivityCard
+                    key={activity.id}
+                    activity={activity}
+                />
+            ))}
+        </Box>
+    )
 }
